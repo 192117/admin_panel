a8d81cf046d376f7832f01acf8bb7f2e4d8a101d
arturkalimullin <noti1996@gmail.com>
2023-05-04 11:22:43 +0300

    Add nginx.conf in docker-compose.yaml
    Add nginx to work with service.

--- movies_admin/config/settings.py
+++ movies_admin/config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise',
+    'corsheaders',
 ]
 
 MY_APPS = [
@@ -58,11 +59,22 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8010',
+    'http://127.0.0.1:8010',
+]
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8010',
+    'http://127.0.0.1:8010',
 ]
 
 ROOT_URLCONF = 'config.urls'
